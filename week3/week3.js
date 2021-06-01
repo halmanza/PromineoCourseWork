524d5488c8e3ae47ddb45a442c42fdb53d41d3fa
Anthony Almanza <anthonya771@gmail.com>
2021-06-01 01:18:19 -0700

    update array names

--- week3/week3.js
+++ week3/week3.js
@@ -1,17 +1,17 @@
-const numArray = [3, 9, 23, 64, 2, 8, 28, 93];
-const nameArray = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+const ages = [3, 9, 23, 64, 2, 8, 28, 93];
+const names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
 const nameLengths = [];
 const secArray = new Array(4).fill(12);
 
 /* lastElement holds the last element in array and updates each time a number is added to the array */
 const lastElement = () => {
-  return numArray[numArray.length - 1];
+  return ages[ages.length - 1];
 };
 /* numAdd adds first and last numbers in array */
 const numAdd = () => {
   let total = 0;
-  numArray.filter((item) => {
-    if (item === numArray[0]) {
+  ages.filter((item) => {
+    if (item === ages[0]) {
       total += item + lastElement();
     }
   });
@@ -19,43 +19,43 @@ const numAdd = () => {
 };
 /* adder pushes number into array */
 const adder = (num) => {
-  numArray.push(num);
+  ages.push(num);
 };
 
 /* for loop that averages the integers in the array */
 
 const loopAverage = () => {
   let total = 0;
-  for (let x = 0; x < numArray.length; x++) {
-    total += numArray[x];
+  for (let x = 0; x < ages.length; x++) {
+    total += ages[x];
   }
 
-  return total / numArray.length;
+  return total / ages.length;
 };
 
 // for loop for adding each names length and averaging the total value
 
 const nameLengthAvg = () => {
   let total = 0;
-  for (let x = 0; x < nameArray.length; x++) {
-    total += nameArray[x].length;
+  for (let x = 0; x < names.length; x++) {
+    total += names[x].length;
   }
-  return total / nameArray.length;
+  return total / names.length;
 };
 
 // for loop for concatenate the names together separated by a space;
 const concatName = () => {
   let name = "";
-  for (let x = 0; x < nameArray.length; x++) {
-    name += " " + nameArray[x];
+  for (let x = 0; x < names.length; x++) {
+    name += " " + names[x];
   }
   return name;
 };
 
 // for loop that takes namesArray and adds the length of each name to the nameLength array
 const addNameLength = () => {
-  for (let x = 0; x < nameArray.length; x++) {
-    nameLengths.push(nameArray[x].length);
+  for (let x = 0; x < names.length; x++) {
+    nameLengths.push(names[x].length);
   }
 };
 
@@ -118,29 +118,29 @@ const selectOdds=(arr)=>{
   }
 
 // Following code is for console logging the results in a formatted method
-console.log(`\n Here is the original array : ${numArray}`);
+console.log(`\n Here is the original ages array : ${ages}`);
 console.log(
   "\n Adding first and last element in array that results in the sum of ->",
   numAdd()
 );
 
 adder(15);
-console.log("\n Now adding the integer 15 to the end of the array\n", numArray);
+console.log("\n Now adding the integer 15 to the end of the array\n", ages);
 
 console.log(
   "\n Here is the new result of adding the first number with the new last array value:\n",
   numAdd()
 );
 
-console.log("\n Average of array values", loopAverage());
+console.log("\n Average of ages array", loopAverage());
 
-console.log("\n Array of names ", nameArray);
+console.log("\n Array of names ", names);
 console.log("\n Average of length of names", nameLengthAvg());
 console.log("\n Names concatenated together: ", concatName());
 
 addNameLength();
 console.log(
-  "\n nameArray's length values added to nameLength array",
+  "\n names's length values added to nameLength array",
   nameLengths
 );
 console.log("\n Sum of length values from nameLength array ", totalNames());
@@ -150,15 +150,15 @@ console.log(
   wordRepeat("hello", 3)
 );
 
-console.log("\n Is sum of numArray greater than 100?  ", intakeArray(numArray));
+console.log("\n Is sum of ages greater than 100?  ", intakeArray(ages));
 
-console.log("\n average of numArray ", averageNumber(numArray));
+console.log("\n average of ages ", averageNumber(ages));
 
 console.log(
-  "\nIs numArray average greater than secArray average: ",
-  compareArray(numArray, secArray)
+  "\nIs ages average greater than secArray average: ",
+  compareArray(ages, secArray)
 );
 
 console.log("\nits hot outside and I have 11 dollars, can I buy a drink?",willBuyDrink(true,11.00))
 
-console.log("\nAll the odd numbers in numArray: ", selectOdds(numArray))
+console.log("\nAll the odd numbers in ages: ", selectOdds(ages))
