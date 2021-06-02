ad78b8ef595ea157c6454829c74e28f1d6b45f54
Anthony Almanza <anthonya771@gmail.com>
2021-06-02 01:34:07 -0700

    update display of nameIntake and fullName functions

--- week4/week4.js
+++ week4/week4.js
@@ -25,6 +25,9 @@ const customCallBackFunction = (str) => {
   console.log(`The last spliced word is ${str}`);
 };
 
+console.log(nameIntake('Anthony','Almanza'))
+console.log(fullName('Captain','America'))
+
 window.setTimeout(()=>alert('Time is up!'), 4000)
 
 window.setInterval(askAreWeThereYet, 12000);
