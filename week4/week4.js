a2e0a63a12750d18ab2cb35ed58110f7a2a18545
Anthony Almanza <anthonya771@gmail.com>
2021-06-02 01:28:08 -0700

    week4 files commit

---
+++ week4/week4.js
@@ -0,0 +1,35 @@
+const splicedValue = ["Hello", "Everyone", "Out", "There"];
+
+function nameIntake(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+
+const fullName = (firstName, lastName) => {
+  return `${firstName} ${lastName}`;
+};
+
+const askAreWeThereYet = () => {
+  return alert("Are we there yet?");
+};
+
+const processSplicedValue = (array, indexNum, response) => {
+  let splicer = array.splice(indexNum, 1);
+  response(splicer);
+
+  if (typeof callback === "function") {
+    callback(splicer);
+  }
+};
+
+const customCallBackFunction = (str) => {
+  console.log(`The last spliced word is ${str}`);
+};
+
+window.setTimeout(()=>alert('Time is up!'), 4000)
+
+window.setInterval(askAreWeThereYet, 12000);
+
+processSplicedValue(splicedValue, 0, console.log);
+processSplicedValue(splicedValue, 1, alert);
+processSplicedValue(splicedValue, 0, () => alert(splicedValue));
+processSplicedValue(splicedValue, -1, customCallBackFunction);
