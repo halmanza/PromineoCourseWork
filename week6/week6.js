e4a6bf6ac0cd0d551ee5dc1dc0006468579a1837
Anthony Almanza <anthonya771@gmail.com>
2021-06-29 00:56:18 -0700

    updated cases for matching card values

--- week6/week6.js
+++ week6/week6.js
@@ -19,10 +19,10 @@ class Deck {
     return this.cards.length;
   }
 
-
+  // Utilized  help with this section from web dev simplified and dev dojo to properly randomize shuffle
   shuffle() {
     for (let x = this.cardLength() - 1; x > 0; x--) {
-      const randomVal = Math.floor(Math.random() * (x - 1));
+      const randomVal = Math.floor(Math.random() * (x + 1));
       const storedVal = this.cards[randomVal];
       this.cards[randomVal] = this.cards[x];
       this.cards[x] = storedVal;
@@ -63,6 +63,7 @@ const compareEachRound = (player1, player2) => {
     points: 0
   };
 
+  //two arrays for player 1 and 2 that contain card suit and card value
   const player1Hand = player1.filter(hand => {
     return hand.cardSuits, hand.cardVal
   });
@@ -70,46 +71,54 @@ const compareEachRound = (player1, player2) => {
     return hand2.cardSuits, hand2.cardVal
   });
 
-
+  // tracker for keeping point score of each round
   let team1Score = 0;
   let team2Score = 0;
 
   for (let x = 1; x < player1Hand.length; x++) {
+    //double check if players both have same amount of starting cards
     switch (player1Hand.length === player2Hand.length) {
-      //case section for player 1 winning
+
+      //case for player 1 winning with number cards against other numbered cards
       case player1Hand[x].cardVal > player2Hand[x].cardVal:
         team1Score++;
-        console.log('Player 1 wins with a ' + ' ' + player1Hand[x].cardSuits + ' ' + player1Hand[x].cardVal+ ' round' +' '+x)
+        console.log('Player 1 wins with a ' + ' ' + player1Hand[x].cardSuits + ' ' + player1Hand[x].cardVal + ' round' + ' ' + x)
         break;
-
+        // case for player 2 winning with number cards against other numbered cards
       case player2Hand[x].cardVal > player1Hand[x].cardVal:
         team2Score++;
-        console.log('Player 2 wins with a ' + ' ' + player2Hand[x].cardSuits + ' ' + player2Hand[x].cardVal + 
-        ' round' +' '+x)
+        console.log('Player 2 wins with a ' + ' ' + player2Hand[x].cardSuits + ' ' + player2Hand[x].cardVal +
+          ' round' + ' ' + x)
         break;
+
+        //case for matching value
+      case player1Hand.cardVal === player2Hand.cardVal:
+        console.log('Draw no points awarded' + ' ' + 'round ' + x);
+        break;
+
+        //Face cards against other cards
 
         //King wins for player 1
       case player1Hand[x] === 'King' && player2Hand[x].cardVal === 'Queen' || player2Hand[x].cardVal === 'Jack' ||
       player2Hand[x].cardVal === 'Ace' || typeof player2Hand[x].cardVal === 'number':
         team1Score++;
         console.log('Player 1 wins with a ' + ' ' + player1Hand[x].cardSuits + ' ' + player1Hand[x].cardVal +
-        ' round' +' '+x)
+          ' round' + ' ' + x)
         break;
 
         //king wins for player 2
       case player2Hand[x] === 'King' && player1Hand[x].cardVal === 'Queen' || player1Hand[x].cardVal === 'Jack' ||
       player1Hand[x].cardVal === 'Ace' || typeof player1Hand[x].cardVal === 'number':
         team2Score++;
-        console.log('Player 2 wins with a ' + ' ' + player2Hand[x].cardSuits + ' ' + player2Hand[x].cardVal+ ' round' +' '+x
-        )
+        console.log('Player 2 wins with a ' + ' ' + player2Hand[x].cardSuits + ' ' + player2Hand[x].cardVal + ' round' + ' ' + x)
         break;
 
         //Queen wins for player 1
-      case player1Hand[x] === 'King' && player2Hand[x].cardVal === 'Queen' || player2Hand[x].cardVal === 'Jack' ||
+      case player1Hand[x] === 'Queen' && player2Hand[x].cardVal === 'Queen' || player2Hand[x].cardVal === 'Jack' ||
       player2Hand[x].cardVal === 'Ace' || typeof player2Hand[x].cardVal === 'number':
         team1Score++;
         console.log('Player 1 wins with a ' + ' ' + player1Hand[x].cardSuits + ' ' + player1Hand[x].cardVal +
-        'round' +' '+x)
+          'round' + ' ' + x)
         break;
 
         //Queen wins for player 2
@@ -117,41 +126,43 @@ const compareEachRound = (player1, player2) => {
       player1Hand[x].cardVal === 'Ace' || typeof player1Hand[x].cardVal === 'number':
         team2Score++;
         console.log('Player 2 wins with a ' + ' ' + player2Hand[x].cardSuits + ' ' + player2Hand[x].cardVal +
-        'round' +' '+x)
+          'round' + ' ' + x)
         break;
 
         //Jack wins for player 1
       case player1Hand[x] === 'Jack' && player2Hand[x].cardVal === 'Ace' || typeof player2Hand[x].cardVal === 'number':
         team1Score++;
         console.log('Player 1 wins with a ' + ' ' + player1Hand[x].cardSuits + ' ' + player1Hand[x].cardVal +
-        'round' +' '+x)
+          'round' + ' ' + x)
         break;
 
         //Jack wins for player 2
       case player2Hand[x] === 'Jack' && player1Hand[x].cardVal === 'Ace' || typeof player1Hand[x].cardVal === 'number':
         team2Score++;
         console.log('Player 2 wins with a ' + ' ' + player2Hand[x].cardSuits + ' ' + player2Hand[x].cardVal +
-        'round' +' '+x)
+          'round' + ' ' + x)
         break;
 
         //Ace wins for player 1
       case player1Hand[x] === 'Ace' && typeof player2Hand[x].cardVal === 'number':
         team1Score++;
         console.log('Player 1 wins a ' + ' ' + player1Hand[x].cardSuits + ' ' + player1Hand[x].cardVal +
-        'round' +' '+x)
+          'round' + ' ' + x)
         break;
 
         //Ace wins for player 2
       case player2Hand[x] === 'Ace' && typeof player1Hand[x].cardVal === 'number':
         team2Score++;
         console.log('Player 2 wins with a ' + ' ' + player2Hand[x].cardSuits + ' ' + player2Hand[x].cardVal +
-        'round' +' '+x)
+          'round' + ' ' + x)
         break;
 
     }
 
   }
 
+
+  // ternary statement for the winning team and total points.
   team1Score > team2Score ?
     (winningPlayer = {
       player: "Player 1",
@@ -184,8 +195,5 @@ player1.initialHand(newGame.deal());
 newGame.shuffle();
 /* shuffle to randomize for each new hand dealt to player */
 player2.initialHand(newGame.deal());
-
-
-
 
 compareEachRound(player1.hand, player2.hand);
