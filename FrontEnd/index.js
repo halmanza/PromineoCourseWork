9580fcfd4241ccb679f59aa66eafd0e189b2fc85
Anthony Almanza <anthonya771@gmail.com>
2021-07-16 14:32:56 -0700

    styling of about and index pages

---
+++ FrontEnd/index.js
@@ -0,0 +1,14 @@
+
+
+function removeStyle(e,styleName){
+    let target= document.getElementById(e);
+    let values= target.children;
+
+    for(let x=0; x < values.length;x++){
+        values[x].classList.remove(styleName);
+    }
+}
+
+paragraphDiv.addEventListener('click',function(e){
+    removeStyle('paragraphDiv','truncate-overflow');
+})
