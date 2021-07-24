b73059cbe96ace8875aca8ab75da8b0dd4d907a5
Anthony Almanza <anthonya771@gmail.com>
2021-07-24 15:16:08 -0700

    initial commit of branch

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,16 +1,23 @@
+let id = 0;
+let tableElm = document.getElementById("mainTable");
+let tableVal= tableElm.children;
+let formValues = document.getElementById("formTableValues");
 
+addRow.addEventListener("click", (e) => {
+    e.preventDefault()
+  let row = tableElm.insertRow(-1);
 
-function removeStyle(e,styleName){
-    let target= document.getElementById(e);
-    let values= target.children;
+  row.setAttribute("id", `item-${id}`);
+  row.insertCell(0).textContent = document.getElementById("firstName").value;
+  row.insertCell(1).textContent = document.getElementById("lastName").value;
+  row.insertCell(2).textContent = document.getElementById("emailItem").value;
+  let actions = row.insertCell(3);
+  actions.append(' ');
+  document.getElementById("firstName").value='';
+  document.getElementById("lastName").value='';
+  document.getElementById("emailItem").value='';
 
-    for(let x=0; x < values.length;x++){
-        values[x].classList.remove(styleName);
-    }
-}
-
-paragraphDiv.addEventListener('click',function(e){
-   removeStyle('paragraphDiv','truncate-overflow');
-})
+ 
+});
 
 
