6fab70597fe910656ab5a00d3cd304733fc8f0a9
Anthony Almanza <anthonya771@gmail.com>
2021-07-25 00:54:43 -0700

    removed update button

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -3,22 +3,20 @@ let tableElm = document.getElementById("mainTable");
 let tableVal = tableElm.children;
 let formValues = document.getElementById("formTableValues");
 
-const createDeleteButton=(id)=>{
-    let btn= document.createElement('button');
-    btn.className='btn btn-primary';
-    btn.id=id;
-    btn.textContent='Delete';
-    btn.onclick=()=>{
-        console.log(`Delete row with id: test-${id}`);
-        let elementToDelete=document.getElementById(`item-${id}`)
-        elementToDelete.parentNode.removeChild(elementToDelete);
-    };
-    return btn;
-}
+const createDeleteButton = (id) => {
+  let btn = document.createElement("button");
+  btn.className = "btn btn-primary";
+  btn.id = id;
+  btn.textContent = "Delete";
+  btn.onclick = () => {
+    console.log(`Delete row with id: item-${id}`);
+    let elementToDelete = document.getElementById(`item-${id}`);
+    elementToDelete.parentNode.removeChild(elementToDelete);
+  };
+  return btn;
+};
 
 addRow.addEventListener("click", (e) => {
-
-  
   e.preventDefault();
   let row = tableElm.insertRow(-1);
 
@@ -26,14 +24,10 @@ addRow.addEventListener("click", (e) => {
   row.insertCell(0).textContent = document.getElementById("firstName").value;
   row.insertCell(1).textContent = document.getElementById("lastName").value;
   row.insertCell(2).textContent = document.getElementById("emailItem").value;
-
-  
   let actions = row.insertCell(3);
-  
+
   actions.append(createDeleteButton(id++));
   document.getElementById("firstName").value = "";
   document.getElementById("lastName").value = "";
   document.getElementById("emailItem").value = "";
-
-  
 });
