8638d188b904b4998b0c00f23303d42bdd0760f2
Anthony Almanza <anthonya771@gmail.com>
2021-08-05 00:05:57 -0700

    streamlined functionality and reduced event listner to group event listener.

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -7,43 +7,51 @@ class TicTacGame {
   createGameField() {
     let field = document.getElementById("gameData");
 
-    this.fieldValues.push(field.children.item(0));
-    this.fieldValues.push(field.children.item(1));
-    this.fieldValues.push(field.children.item(2));
+    this.fieldValues.push(field.children[0]);
+    this.fieldValues.push(field.children[1]);
+    this.fieldValues.push(field.children[2]);
   }
 
-  checkForWinner() {
-    const win1x= ['X',13,'X',13,'X',13]
-    
-    let xValues = this.fieldValues.map((item) => {
-      return item.textContent.match(/x/i);
+  fieldValueCheck() {
+    let winDiagnol1 = [
+      { id: "row1_1", value: "X" },
+      { id: "row2_2", value: "X" },
+      { id: "row3_3", value: "X" },
+    ];
+
+    this.fieldValues.flatMap((item) => {
+      this.totalVal.push({
+        id: item.firstElementChild.id,
+        value: item.firstElementChild.textContent,
+      });
+      this.totalVal.push({
+        id: item.lastElementChild.previousElementSibling.id,
+        value: item.lastElementChild.previousElementSibling.textContent,
+      });
+      this.totalVal.push({
+        id: item.lastElementChild.id,
+        value: item.lastElementChild.textContent,
+      });
     });
 
-    let filterVal = xValues.filter((item) => {
-      return item;
+    let result = this.totalVal.filter((item) => {
+      return winDiagnol1.some((item2) => {
+        item.id === item2.id && item.value === item2.value;
+      });
     });
 
-    let xKeys = filterVal.find((item) =>
-      this.totalVal.push(item[0], item.index)
-    );
-    console.log(xKeys)
-    console.log(this.totalVal)
-    let firstWinX= this.totalVal.length === win1x.length &&
-    this.totalVal.every((elm)=>{
-      return win1x.includes(elm);
-    })
+    console.log(Boolean(result));
 
-    switch (this.totalVal.length > 0){
-        case firstWinX:
-          console.log(true);
-          break;
-        default:
-          console.log('didnt work');
-    }
+    // switch(this.fieldValues.length > 0){
+    //   case diagnolCheck1:
+    //     console.log(true)
+    //   default:
+    //     console.log('Nope')
+    // }
   }
 
-  resetGameValues(){
-    this.totalVal=[];
+  resetGameValues() {
+    this.totalVal = [];
   }
 }
 
@@ -54,51 +62,71 @@ class TicTacPlayer {
 
   /* Method that creates even listener on all the td elements in the div#gameData element, It 
   fills the td with the players selected value of X or O */
-  selectSquare(color) {
-    let item = gameData.querySelectorAll("td");
 
-    for (let elm of item) {
-      if (elm.textContent == "free") {
-        elm.addEventListener("click", () => {
-          elm.textContent = this.player;
-          elm.style.background = color;
-        });
-      }
+  selectSquare(evt, color) {
+    if (evt.target.closest("td")) {
+      evt.target.textContent = this.player;
+      evt.target.style.background = color;
     }
   }
 }
 
-// Utilitiy functionf or getting element ID
+// Utilitiy function for getting element ID
 function getId(id) {
   return document.getElementById(id);
+}
+
+function observeCallback(mutations) {
+  for (let mute of mutations) {
+    if (mute.type === "childList") {
+      newGame.resetGameValues();
+      // newGame.fieldValues.flatMap((item) => console.log(item));
+      newGame.fieldValueCheck();
+      let testValue = newGame.totalVal.filter((item) => {
+        return item;
+      });
+      console.log(testValue);
+    }
+  }
 }
 
 const newGame = new TicTacGame();
 newGame.createGameField();
 
+const watchNode = document.getElementById("gameData");
+const oberserverObject = {
+  childList: true,
+  subtree: true,
+};
+let observer = new MutationObserver(observeCallback);
+observer.observe(watchNode, oberserverObject);
+
 const circlePlayer = new TicTacPlayer("O");
 const xPlayer = new TicTacPlayer("X");
 
-getId("player-x").addEventListener("click", () => {
-  xPlayer.selectSquare("darkred");
- 
-});
+getId("buttonGroup").addEventListener("click", (e) => {
+  let target = e.target;
+  switch (target.id) {
+    case "player-x":
+      watchNode.addEventListener("click", (e) => {
+        xPlayer.selectSquare(e, "darkred");
+      });
+      break;
+    case "player-0":
+      watchNode.addEventListener("click", (e) => {
+        circlePlayer.selectSquare(e, "lightsalmon");
+      });
+      break;
+    case "resetGame":
+      let resetItems = gameData.querySelectorAll("td");
+      for (let item of resetItems) {
+        item.textContent = "free";
+        item.style.background = "#212529";
+      }
 
-getId("player-0").addEventListener("click", () => {
-  circlePlayer.selectSquare("darksalmon");
-});
-
-getId("resetGame").addEventListener("click", () => {
-  let resetItems = gameData.querySelectorAll("td");
-  for (let item of resetItems) {
-    item.textContent = "free";
-    item.style.background = "#212529";
+      newGame.resetGameValues();
+      break;
+    default:
+      console.log("Next Round");
   }
-  newGame.resetGameValues();
 });
-
-getId("gameData").addEventListener("click", () => {
-  newGame.checkForWinner();
-  
-});
-
